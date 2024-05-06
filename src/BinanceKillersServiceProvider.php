8e3d64576baf92217eb0a68e5e8ab49b99b5d8ca
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-05-06 08:25:00 +0200

    Initial commit

---
+++ src/BinanceKillersServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brunocfalcao\BinanceKillers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BinanceKillersServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
